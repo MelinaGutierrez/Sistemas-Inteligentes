8aae09491a0a6d4bdcd8100457b71d9734f08897
Melina Gutierrez <melina.gutierrez@ucb.edu.bo>
2024-04-14 23:06:57 -0400

    PCR crear clase de esquinas para los lados

--- Mini Proyecto Cubo Rubik/cubo.py
+++ Mini Proyecto Cubo Rubik/cubo.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as pl
+import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
 
 class Cubito:
@@ -6,4 +7,27 @@ class Cubito:
         self.centro=centro
         self.distancia=distancia
         self.color= color
+    
+    def obtener_esquinas(self):
+        x, y, z = self.centro
+        semiarista = self.distancia/2
+        esquinas = np.array([
+            # + , + , + 
+            # + , + , - 
+            # + , - , + 
+            # + , - , -
+            # - , + , + 
+            # - , + , -
+            # - , - , + 
+            # - , - , -
+            [x+semiarista, y+semiarista, z+semiarista],
+            [x+semiarista, y+semiarista, z-semiarista],
+            [x+semiarista, y-semiarista, z+semiarista],
+            [x+semiarista, y-semiarista, z-semiarista],
+            [x-semiarista, y+semiarista, z+semiarista],
+            [x-semiarista, y+semiarista, z-semiarista],
+            [x-semiarista, y-semiarista, z+semiarista],
+            [x-semiarista, y-semiarista, z-semiarista]
+        ])
+        return esquinas
     
