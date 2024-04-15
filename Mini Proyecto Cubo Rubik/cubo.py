2f0766403b6a43ade8c1a298a51f74e4ec36e1dc
Melina Gutierrez <melina.gutierrez@ucb.edu.bo>
2024-04-14 22:41:05 -0400

    PCR Crear la clase cubo e inicializar valores

---
+++ Mini Proyecto Cubo Rubik/cubo.py
@@ -0,0 +1,9 @@
+import matplotlib.pyplot as pl
+from mpl_toolkits.mplot3d import Axes3D
+
+class Cubito:
+    def __init__(self, centro, distancia, color):
+        self.centro=centro
+        self.distancia=distancia
+        self.color= color
+    
